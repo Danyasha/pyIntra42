308d915154234c3004731a1d73b80dc912c8fb72
Danyasha <daniiltikhomirov1998@yandex.ru>
2018-12-24 17:54:52 +0300

    Добавлена возможность запросить топ

--- getTop.py
+++ getTop.py
@@ -5,10 +5,6 @@ def main():
     fd = open(argv[1], 'r')
     users = json.load(fd)
     fd.close()
-    users = sorted(users, key=lambda x: -x['lvl'])
-    fd = open(argv[1], 'w')
-    json.dump(users, fd, ensure_ascii=False, sort_keys=True, indent=4)
-    fd.close()
     users = users[0:15]
     formattedUsers = []
     for k in range(0, len(users)):
@@ -16,7 +12,7 @@ def main():
         for i in users[k].get('users'):
             placeGuys += "https://profile.intra.42.fr/users/" + i + "\n"
         placeGuys = placeGuys[0:-1]
-        formattedUsers.append("top-%s c lvl - %s:\n%s" %(str(k), str(round(users[k].get('lvl'), 2)), placeGuys))
+        formattedUsers.append("top-%s c lvl - %s:\n%s" %(str(k) + 1, str(round(users[k].get('lvl'), 2)), placeGuys))
     for i in formattedUsers:
         print(i)
 if __name__ == "__main__":
--- topladdera228.py
+++ topladdera228.py
@@ -19,22 +19,41 @@ def findLogin(login, users):
     k = 0
     for i in users:
         if login in i.get("users"):
-            howMuchUsers = "\nделит место с %s людьми"%str(len(i.get("users")))
+            howMuchUsers = "\nделит место с %s людьми"%str(len(i.get("users")) - 1)
             answer = "%s:\n%s в общем рейтинге с %s уровнем,"%(login, str(k), str(round(i.get("lvl"), 2))) + howMuchUsers
             answer += "\nПеред: %s\nПосле: %s"%(str(i.get("before")), str(i.get("after")))
             return(answer)
         k+=1
     return("Проверь правильность написания логина")
+def getTop(users):
+    users = users[0:15]
+    formattedUsers = []
+    temp = ""
+    for k in range(0, len(users)):
+        placeGuys = ""
+        for i in users[k].get('users'):
+            placeGuys += "https://profile.intra.42.fr/users/" + i + "\n"
+        formattedUsers.append("top-%s c lvl - %s:\n%s" %(str(k + 1) , str(round(users[k].get('lvl'), 2)), placeGuys))
+    temp = temp.join(formattedUsers)
+    return (temp)
 longpoll = VkLongPoll(vk_session)
 users = getUsers()
 for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW and event.from_chat and event.text and event.text.split()[0].lower() == "!рейтинг":
-        try:
-            login = event.text.split()[1].lower()
-            if login != "vice-wra":
-                vk.messages.send(peer_id=2000000000 + event.chat_id, message=findLogin(login, users), random_id = "0")
+    if event.type == VkEventType.MESSAGE_NEW and event.text and event.text.split()[0].lower() == "!рейтинг":
+        if event.from_chat:
+            try:
+                login = event.text.split()[1].lower()
+                if login != "vice-wra":
+                    vk.messages.send(peer_id=2000000000 + event.chat_id, message=findLogin(login, users), random_id = "0")
+                else:
+                    vk.messages.send(peer_id=2000000000 + event.chat_id, message="undefined. Kicked for harassment", random_id = "0")
+            except IndexError:
+                print("6 утра, мне было лень")
+        elif event.to_me:
+            if (len(event.text.split()) >= 2):
+                login = event.text.split()[1].lower()
+                vk.messages.send(user_id = event.user_id, message=findLogin(login, users), random_id = "0")
             else:
-                vk.messages.send(peer_id=2000000000 + event.chat_id, message="undefined. Kicked for harassment", random_id = "0")
-        except IndexError:
-            print("6 утра, мне было лень")
+                vk.messages.send(user_id = event.user_id, message=getTop(users), random_id = "0")
+
 
